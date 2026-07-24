cff90c9c904aac42122124474cbacd2b0c3c131d
NOTZ112 <liza3x3i@gmail.com>
2026-07-24 21:36:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,3 +9,8 @@ print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
+age = int(input("നിങ്ങളുടെ വയസ്സ് എത്ര? "))
+
+print("നിങ്ങളുടെ വയസ്സ്:", age)
+
+print("അടുത്ത വർഷം നിങ്ങളുടെ വയസ്സ്:", age + 1)
